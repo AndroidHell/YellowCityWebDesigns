50830ee3a8bb19453d9c4086bbca0c1d4fe5aedc
AndroidHell <ralkkai1337@gmail.com>
2024-05-25 08:27:51 -0500

    fixing drawer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
+  output: "server",
+  adapter: netlify()
 });
