ad6178b5f46c7e8befca18b0a7415dd55ece7d2e
AndroidHell <ralkkai1337@gmail.com>
2024-05-28 09:19:47 -0500

    started work on components examples

---
+++ src/pages/componentExamples/headers/navigationItemsExamples.ts
@@ -0,0 +1,27 @@
+export const navigationItemsExamples = [
+  {
+    title: "Home",
+    path: "#",
+  },
+  {
+    title: "Services",
+    path: "#",
+  },
+  {
+    title: "Components ⌄",
+    path: "#",
+  },
+
+  {
+    title: "About",
+    path: "#",
+  },
+  {
+    title: "Blog",
+    path: "#",
+  },
+  {
+    title: "Contact Us",
+    path: "#",
+  },
+];
