50325767a7d43d81a95a1bc611d15dcc3a1b164a
AndroidHell <ralkkai1337@gmail.com>
2024-07-11 10:29:13 -0500

    finishing up blog

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = "Yellow City Web Design";
 export const SITE_DESCRIPTION =
   "This is my AstroJS Master Template. The goal is to simplify my web development using a scaffolding of unique pages that will be injected with components. The components will be self contained. Will expand on this as the project takes shape.";
+export const GENERATE_SLUG_FROM_TITLE = true;
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,39 @@
-import { defineCollection, z } from 'astro:content';
-
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+import { z, defineCollection } from "astro:content";
+const blogSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.string().optional(),
+  heroImage: z.string().optional(),
+  badge: z.string().optional(),
+  tags: z
+    .array(z.string())
+    .refine((items) => new Set(items).size === items.length, {
+      message: "tags must be unique",
+    })
+    .optional(),
 });
 
-export const collections = { blog };
+const newsSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  custom_link_label: z.string(),
+  custom_link: z.string().optional(),
+  updatedDate: z.coerce.date(),
+  pricing: z.string().optional(),
+  oldPricing: z.string().optional(),
+  badge: z.string().optional(),
+  checkoutUrl: z.string().optional(),
+  heroImage: z.string().optional(),
+});
+
+export type BlogSchema = z.infer<typeof blogSchema>;
+export type StoreSchema = z.infer<typeof newsSchema>;
+
+const blogCollection = defineCollection({ schema: blogSchema });
+const storeCollection = defineCollection({ schema: newsSchema });
+
+export const collections = {
+  blog: blogCollection,
+  store: storeCollection,
+};
