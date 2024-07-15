8c20eea6e7edb186fb1393aef4fd87ec83a914a6
AndroidHell <ralkkai1337@gmail.com>
2024-07-15 14:13:32 -0500

    adding heroImage default to collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,8 +5,11 @@ const blogSchema = z.object({
   pubDate: z.coerce.date(),
   updatedDate: z.union([z.string().length(0), z.coerce.date()]).optional(),
   heroImage: z.preprocess(
-    (value) => (value === "" ? "/assets/images/placeholder.jpg" : value),
-    z.string().optional(),
+    (value) =>
+      value === "" || value === undefined
+        ? "/assets/images/placeholder.jpg"
+        : value,
+    z.string(),
   ),
   badge: z.string().optional(),
   tags: z
