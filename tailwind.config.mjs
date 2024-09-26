cc7c516c035758f82b579d3d3a7a932503ad298a
AndroidHell <ralkkai1337@gmail.com>
2024-09-26 13:14:08 -0500

    testing dynamic header images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -72,10 +72,6 @@ module.exports = {
         "Noto Color Emoji",
       ],
     },
-    backgroundImage: {
-      page_header_light: "url('/page-header-light.webp')",
-      page_header_dark: "url('/page-header-dark.webp')",
-    },
   },
   plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
